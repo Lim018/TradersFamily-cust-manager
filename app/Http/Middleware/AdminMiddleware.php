66f2df456486f44f951b130ff7f3d9cd6bdc5e2a
alim <149599272+Lim018@users.noreply.github.com>
2025-07-22 10:52:11 +0700

    add middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        if (!$user || !$user->isAdmin()) {
+            abort(403, 'Unauthorized');
+        }
+        return $next($request);
+    }
+}
